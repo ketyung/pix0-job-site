9c1807f9f5f9e3d90e6fd885d4c53bf719f5f484
techchee <ketyung@techchee.com>
2024-04-07 10:49:49 +0800

    update

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { createGoogleCredential } from "../dbs/user";
+import { sha256 } from "@/utils/enc";
+
+// refer here for Google Sign In
+// https://next-auth.js.org/providers/google
+// https://github.com/nextauthjs/next-auth/tree/v4/packages/next-auth
+// https://www.telerik.com/blogs/how-to-implement-google-authentication-nextjs-app-using-nextauth
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        })
+    ],
+
+    callbacks: {
+        
+        async signIn({ account, profile }) {
+          if (account !== null && account.provider === "google") {
+             //console.log("profile.is:::", profile);
+             let stat = await createGC(profile, account);
+
+             return stat.status;
+          }
+          return false; // Do different verification for other providers that don't have `email_verified`
+        },
+
+        async session({ session, user, token }) {
+            //console.log("user:::", user, token, session);
+            let sAccId= sha256(token.sub);
+            //console.log("sAcc.id::", sAccId);
+            let newSession : any = {...session, accountId : sAccId };
+
+            //console.log("n.sess:", newSession);
+            return newSession
+        },
+    },
+
+    pages: {
+        signIn: '/api/auth/google', // Redirect to Google sign-in
+    },
+   
+});
+
+
+async function createGC(profile : any , account : any) : Promise<{ status:boolean, encAccountId? : string}>{
+
+    try {
+        return await createGoogleCredential(profile, account);
+
+    }
+    catch(e: any){
+        console.log("error.credting.gc::",e );
+        return {status : false }; 
+    }
+    
+}
