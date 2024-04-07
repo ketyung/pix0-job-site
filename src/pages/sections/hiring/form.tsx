392cbfaeb15cc9529b58bd5bc195ca22cd05699b
techchee <ketyung@techchee.com>
2024-04-07 17:39:09 +0800

    update

---
+++ src/pages/sections/hiring/form.tsx
@@ -0,0 +1,216 @@
+import FieldLabel from "@/components/FieldLabel"
+import { JobPostTypes, JobPostType } from "@/models"
+import { Select, Input, TextArea, Button } from "pix0-core-ui"
+import { CiCircleInfo } from "react-icons/ci";
+import { useState, useEffect } from "react";
+import { JobPost } from "@prisma/client";
+import { isBlank } from "@/utils";
+import { toast } from "react-toastify";
+import { createJobPost, updateJobPost, getJobPost , genProdDesc} from "@/service";
+import { ntb } from "@/utils";
+import { BeatLoader } from "react-spinners";
+import { BsMarkdown } from "react-icons/bs";
+import { CiText } from "react-icons/ci";
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+import MarkdownIt from 'markdown-it';
+
+
+type props = {
+
+   
+    title? : string, 
+
+    fromInv? : boolean,
+
+    refresh? : () => void, 
+
+    isEditMode? : boolean,
+
+    editRowId? : string, 
+}
+
+
+export const DEFAULT_JOBPOST : any= {
+
+    code : "",
+    name: "",
+    description: "",
+    jobpostType : JobPostType.JobPost,
+    unitPrice: 0,
+    createdBy: "",
+    dateCreated : new Date(),
+    dateUpdated : new Date(),
+    forOnlineStore: 'N',
+    companyId : "",
+    qoh:0,
+
+}
+
+export default function Form({ title, fromInv, isEditMode, refresh, editRowId} :props) {
+
+    const mdParser = new MarkdownIt(/* Markdown-it options */);
+   
+    const [processing, setProcessing] = useState(false);
+
+    const [jobpost, setJobPost] = useState<JobPost>(DEFAULT_JOBPOST);
+
+    const [generating, setGenerating] = useState(false);
+
+    const [viewMarkDown, setViewMarkDown] = useState(false);
+
+    const saveJobPostNow = async () =>{
+
+        if ( jobpost === undefined){
+            toast.error(`JobPost is undefined!`);
+            return ;
+            
+        }
+
+        //console.log("JobPost:::", jobpost);
+        if (isBlank(jobpost?.name) ){
+            toast.error(`JobPost Name Must NOT be blank!`);
+            return ;
+        }
+        
+        setProcessing(true);
+
+        let n = isEditMode ? await updateJobPost(jobpost, (e)=>{
+            toast.error(e.message);
+        }) : await createJobPost(jobpost, (e)=>{
+            toast.error(e.message);
+        });
+
+        setProcessing(false);
+        if ( n ){
+            toast.info(isEditMode ? 'Successfully Updated' : 'New jobpost created successfully!');
+            if ( refresh) {
+                refresh();
+            }
+            if ( !isEditMode) setJobPost(DEFAULT_JOBPOST);
+            return;
+        }
+    }
+    
+    const genProdDescNow = async () =>{
+
+        if ( jobpost.name?.trim() === ""){
+            toast.error("Please enter the name of the jobpost first");
+            return; 
+        }
+        setGenerating(true);
+        let text = await genProdDesc(jobpost.name ?? "My First Product JobPost");
+        setJobPost({...jobpost, description : text});
+        setGenerating(false);
+    }
+
+    useEffect(()=>{
+
+        if (isEditMode && editRowId) {
+
+            getJobPost(editRowId, (e)=>{
+                toast.error(e.message);
+            } ).then (i=>{
+                if ( i ){
+                    setJobPost(i);
+                }
+            });
+        }else {
+            setJobPost(DEFAULT_JOBPOST);
+        }
+
+    },[editRowId,isEditMode]);
+
+
+    return <div className="mt-2 border border-gray-300 rounded p-2 w-full lg:mb-2 mb-20">
+        <div className="mt-2 mb-2 text-left p-1 font-bold">
+        {title ?? (isEditMode ? "Edit JobPost" : "Add New JobPost")}        
+        </div>
+        <div className="mt-2 mb-2 lg:flex text-left">
+            <FieldLabel title="JobPost Code" className="lg:w-1/5 w-full">
+                <Input placeholder="Code" onChange={(e)=>{
+                    setJobPost({...jobpost, code: e.target.value});
+                }} value={ntb(jobpost.code)} className="w-full" icon={<CiCircleInfo className="mb-2"/>}/>
+            </FieldLabel>
+            <FieldLabel title="Name" className="lg:w-2/5 w-full mt-2 lg:mt-0 lg:ml-2">
+                <Input placeholder="Name" onChange={(e)=>{
+                    setJobPost({...jobpost, name : e.target.value});
+                }} value={ntb(jobpost.name)} className="w-full" icon={<CiCircleInfo className="mb-2"/>}/>
+                </FieldLabel>
+        </div>
+        <div className="mt-2 mb-2 text-left">
+        <FieldLabel title={<div className="flex"><div className="mt-1">Description</div> 
+        <Button className="border border-gray-300 rounded p-1 ml-2 mb-1 w-32" disabled={generating}
+        onClick={async (e)=>{
+            e.preventDefault();
+            await genProdDescNow();
+        }}>{generating ? <BeatLoader size={6} color="#888"/> : <>Generate By AI</>}</Button>
+        <Button className="ml-4" onClick={(e)=>{
+            e.preventDefault();
+            setViewMarkDown(!viewMarkDown);  
+        }}>{viewMarkDown ? <CiText  title="Change Back To Plain Text Editor" className="w-5 h-5"/> 
+        : <BsMarkdown title="Edit In Mark Down Editor" className="w-5 h-5"/>}</Button>
+        
+        </div>} className="lg:w-4/5 w-full">
+            { viewMarkDown ? <MdEditor value={ntb(jobpost.description)} style={{ height: '200px' }} 
+            renderHTML={text => mdParser.render(text)} onChange={(e)=>{
+                setJobPost({...jobpost, description : e.text});
+            }} view={{
+                md: true, // Set to true to display Markdown content
+                html: false, // Set to true to display rendered HTML content
+                menu: true, // Set to true to hide the toolbar by default
+            }}/> : <TextArea rows={5} value={ntb(jobpost.description)} 
+                placeholder="Add a short description such as provide additional information about this jobpost"
+            onChange={(e)=>{
+                setJobPost({...jobpost, description : e.target.value});
+            }}/>}
+        </FieldLabel>
+        </div>
+        <div className="mt-2 mb-2 text-left">
+            <FieldLabel title="JobPost Type" className="lg:w-3/5 w-full">
+                <Select value={ntb(jobpost.jobpostType)} options={JobPostTypes.map(i=>{
+                    return {value : i, label: i}
+                })} onChange={(e)=>{
+                    let selType = JobPostTypes.filter(i=>{
+                        return i === e.target.value;
+                    });
+
+                    if ( setJobPost && selType.length > 0 ) setJobPost({...jobpost, jobpostType:selType[0]});    
+                }}/>          
+            </FieldLabel>
+        </div>
+
+        <div className="mt-2 mb-2 lg:flex text-left">
+            <FieldLabel title="Unit Price" className="lg:w-3/12 w-full">
+                <Input placeholder="Unit Price" type="number" onChange={(e)=>{
+                      if ( setJobPost) setJobPost({...jobpost, unitPrice :parseFloat(e.target.value)});
+                }} value={jobpost?.unitPrice?.toFixed(2)} className="w-full" icon={<CiCircleInfo className="mb-2"/>}/>
+            </FieldLabel>
+            {fromInv &&
+            <FieldLabel title="Invoice Qty" className="lg:w-3/12 w-full lg:ml-2">
+                <Input placeholder="Inv Qty" type="number" onChange={(e)=>{
+                     
+                }} className="w-full" icon={<CiCircleInfo className="mb-2"/>}/>
+            </FieldLabel>}
+            <FieldLabel title="Qty On Hand" className={`lg:w-3/12 w-full mt-2 lg:mt-0 lg:ml-2`}>
+                <Input placeholder="QOH" onChange={(e)=>{
+                    setJobPost({...jobpost, qoh :parseInt(e.target.value)});
+                }} value={jobpost.qoh !== null ? jobpost.qoh : ""} className="w-full" type="number" icon={<CiCircleInfo className="mb-2"/>}/>
+                </FieldLabel>
+        </div>
+
+        <div className="mt-2 mb-2 lg:flex">
+            <Button disabled={processing} className="border border-gray-300 w-48 flex rounded justify-center bg-gray-300 dark:bg-gray-600 py-1"
+            onClick={async (e)=>{
+                e.preventDefault();
+                await saveJobPostNow();
+
+            }}>
+             {processing ? <BeatLoader size={8} color="#ddd"/> :  <>{!isEditMode ? "Create JobPost" : "Update JobPost"}</>}
+            </Button>
+        </div>
+    </div>
+
+    
+}
---
+++ src/pages/sections/hiring/list.tsx
@@ -0,0 +1,90 @@
+import { SearchResult } from "@/models"
+import { useEffect, useState, useCallback, useMemo} from "react"
+import { getItems } from "@/service";
+import { Pagination, Input } from "pix0-core-ui";
+import { STANDARD_RES_ROWS_PER_PAGE } from "@/models";
+import { CiSearch } from "react-icons/ci";
+import Row from "./row";
+import BeatLoader from "react-spinners/BeatLoader";
+
+type props = {
+
+    reloadCount? : number,
+
+    onEdit?: (id? : string) => void, 
+}
+
+export default function List({reloadCount, onEdit} :props) {
+
+    const [searchResult, setSearchResult] = useState<SearchResult>({results:[]});
+
+    const [page, setPage] = useState(1);
+
+    const [searchStr, setSearchStr] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
+    const rowsPerPage = STANDARD_RES_ROWS_PER_PAGE;
+
+    const refreshResult =  useMemo(() => async (searchString? : string, pageNum? : number ) => {
+        setLoading(true);
+        let s = await getItems(searchString ?? searchStr, '-', '-', pageNum ?? page , rowsPerPage);
+        setSearchResult(s);
+        setLoading(false);
+    }, [searchStr, page, rowsPerPage]);
+
+    const fetchData = useCallback(async () => {
+       await refreshResult();
+    }, [ refreshResult]);
+    
+
+    useEffect(()=>{
+      fetchData();
+    },[fetchData,reloadCount]);
+
+    return <div className="overflow-x-auto mx-2 mb-20 text-sm">
+    <div>
+    <Input className="lg:w-3/5 w-full mb-2" placeholder="Enter item code or name to search" 
+      rightIcon={
+        loading ? <BeatLoader className="lg:ml-4 w-10 h-4" color="#aaa" size={8}/> :
+      <CiSearch className="w-5 h-5 mb-1 cursor-pointer" onClick={async ()=>{
+          await refreshResult();
+      }}/>}
+      onChange={async (e)=>{
+          setSearchStr(e.target.value);
+          setPage(1);
+          await refreshResult(e.target.value,1);
+      }}/>
+    </div>
+    <table className="table-auto min-w-full divide-y divide-gray-200">
+      <thead>
+       
+        <tr className="dark:bg-gray-800 bg-gray-100 border-b border-gray-300 text-xs font-bold dark:text-gray-100 text-gray-500 uppercase">
+          <th className="hidden lg:inline-block text-center py-2 px-2" style={{maxWidth:"15%"}}>No.</th>
+          <th className="hidden lg:inline-block px-1 text-left py-2" style={{maxWidth:"10%"}}>Code</th>
+          <th className="px-6 text-left py-2" style={{maxWidth:"35%"}}>Name</th>
+          <th className="hidden lg:inline-block px-1 text-left py-2">Type</th>
+          <th className="px-6 text-left py-2">QOH</th>
+          <th className="px-6 text-left py-2 text-center">Action</th>
+        </tr>
+      </thead>
+      <tbody className="dark:bg-gray-800 bg-white divide-y dark:divide-gray-600 divide-gray-200">
+      {
+            searchResult.results?.map((r,i)=>{
+                    return <Row row={r} key={`ItemRow_${i}`} index={(i+1) + ((page -1) * rowsPerPage) } refresh={refreshResult} onEdit={onEdit}/>
+            })
+      }
+      { (searchResult.results.length ?? 0) > 0 &&
+      <tr>
+          <th colSpan={6}>
+            <Pagination currentPage={page} totalPages={searchResult.totalPages ?? 0}
+            onPageChange={async (p)=>{
+                setPage(p);
+                await refreshResult(searchStr, p);
+            }} total={searchResult.total}/>
+          </th>
+      </tr>}
+      </tbody>
+    </table>
+    </div>
+}
