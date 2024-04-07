6c0ae9e291444b53eb5aae4f29398fc5991dcbe3
techchee <ketyung@techchee.com>
2024-04-07 11:35:55 +0800

    update

--- src/pages/layout2.tsx
+++ src/pages/layout2.tsx
@@ -26,7 +26,7 @@ const DefaultMain = () =>{
                 <p className='norm-text'>Your partner in creating innovative web and AI solutions.</p>
                 </div>
             </div>
-            <div className='container'>
+            <div className='container dark:bg-gray-800'>
                 <FieldLabel title="Job Search">
                     <Input className='lg:w-3/5 w-full'/>
                 </FieldLabel>
@@ -44,7 +44,7 @@ export default function Layout({ title, description, children} : props) {
     return <><header className='dark:bg-gray-800 bg-top-bar-blue dark:text-gray-100 text-gray-800 shadow-xl 
         border-width:1px dark:border-0 border border-gray-300 dark:border-gray-800 flex'>
             <div className='bg-gray-700 rounded text-gray-100 py-1 px-4 text-sm font-bold m-2 hover:bg-gray-900 cursor-pointer'>Post A JOB</div>
-            <ThemeToggle/>
+            <ThemeToggle iconLightTextColor='#ffa'/>
 
         </header>
         { children ? <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-100 text-gray-900 min-h-screen h-full pt-4 pb-4">
