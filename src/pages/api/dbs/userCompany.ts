33e1823942c80351e26d8879aa8aa4d355ec26d4
ketyung <ketyung@techchee.com>
2024-04-09 13:47:26 +0800

    update

--- src/pages/api/dbs/userCompany.ts
+++ src/pages/api/dbs/userCompany.ts
@@ -1,11 +1,10 @@
 import { UserCompany } from "@prisma/client";
 import prisma from '../db';
-
+import { isBlank } from "@/utils";
+import { SearchResult } from "@/models";
 
 export async function getUserCompany(userId : string ) :Promise<UserCompany|null> {
     try {
-  
-        
   
         const userCompany = await prisma.userCompany.findFirst({
           where: {
@@ -21,3 +20,160 @@ export async function getUserCompany(userId : string ) :Promise<UserCompany|null
         return null;
     }
   }
+
+
+
+
+export async function createCompany(userId: string,company  : UserCompany) {
+
+    if ( isBlank(company.name)) {
+        throw new Error("Title of job post must not be blank!");
+    }
+
+    try {
+
+        let nComp : any = {...company, userId : userId, dateCreated : new Date()};
+
+        let newCompany = await prisma.userCompany.create({data: nComp});
+
+        const cuid = newCompany.id;
+
+        return {cuid : cuid, status : 1}; 
+
+    }
+    catch(e : any){
+        throw e ;
+    }
+ 
+}
+
+
+export async function updateCompany(userId: string,userCompany : UserCompany) {
+
+
+   
+  if ( isBlank(userCompany.name )) {
+      throw new Error("Name of company must not be blank!");
+  }
+
+  try {
+
+    
+     
+      let wh :any =  {
+          userId : userId, 
+          companyId : userCompany?.id
+      };
+              
+      const existingUserCompany = await prisma.userCompany.findFirst({
+          where: wh 
+      });
+
+      if (existingUserCompany) {
+          const updatedUserCompany = await prisma.userCompany.update({
+              where: wh,
+              data: userCompany
+          });
+
+          
+          return (updatedUserCompany.id === userCompany.id && updatedUserCompany.id === userCompany?.id);
+      } else {
+
+          throw Error('UserCompany NOT found!');
+      }
+
+
+
+  }
+  catch(e : any){
+      throw e ;
+  }
+ 
+}
+
+
+
+
+export async function getCompanies(userId: string, keyword?: string, orderBy? : string, 
+  ascOrDesc? : string, offset: number = 0, limit: number = 10) :Promise<SearchResult> {
+  
+    
+    let whereClause: any = {
+        where: {
+            userId,
+        },
+    };
+
+    if (keyword && keyword.trim()!== '-') {
+        whereClause = {
+            where: {
+                userId ,
+                OR: [
+                    { code: { contains: keyword } },
+                    { name: { contains: keyword } },
+                ],
+            },
+        };
+    }
+
+
+    let ordBy : any =  {
+        dateCreated : 'desc'
+    };
+
+    if ( orderBy ){
+        ordBy = {
+            [`${orderBy}`]: ascOrDesc ?? 'asc'
+        };
+    }
+
+    const comps = await prisma.jobPost.findMany({
+        ...whereClause,
+        skip: offset,
+        take: limit,
+        orderBy: ordBy,
+    });
+
+
+    let total = await getCount(whereClause);
+
+    return {results : comps, total};
+}
+
+
+
+async function getCount(where: any ): Promise<number> {
+  
+    const count = await prisma.userCompany.count({...where });
+    return count;
+}
+
+
+
+
+export async function deleteCompany(userId: string, id : string ) :Promise<boolean> {
+
+  try {
+
+      let whereClause: any = {
+          where: {
+              id ,
+              userId 
+          },
+      };
+
+  
+      const deletedRow = await prisma.userCompany.delete({
+          ...whereClause,
+      });
+
+      if ( deletedRow === null) return false;
+      
+      return deletedRow.id === id && deletedRow.userId === userId;
+
+    } 
+    catch (error) {
+      console.error('Error deleting row:', error);
+      return false;   
+    }
+}
---
+++ src/pages/api/modules/company.ts
@@ -0,0 +1,211 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import bodyParser from "body-parser";
+import { getPageOffsetAndLimit, toTotalPages } from '@/utils';
+import { createCompany, getUserCompany, updateCompany, getCompanies, deleteCompany} from '../dbs/userCompany';
+
+const jsonParser = bodyParser.json();
+
+export async function handler(req: NextApiRequest, res: NextApiResponse, userId? : string ) {
+
+    if (req.method === 'GET') {
+        await handleGet(req, res, userId);
+    }else if (req.method ==='POST'){
+        await handlePost(req, res, userId);
+    }else if (req.method ==='DELETE'){
+        await handleDelete(req, res, userId);
+    }
+}
+
+
+
+
+async function handleGet (req: NextApiRequest,  res: NextApiResponse, userId? : string ) {
+    const { path } = req.query;
+
+    if (Array.isArray(path) && path.length > 1) {   
+
+        const param1 = path[1];
+
+        if (param1!== undefined && param1 !== null ){
+        
+            if ( param1 === 'search'){
+                let keyword = path[2];
+                let orderBy = path[3];
+                let ascOrDesc = path[4];
+                let pageNum = parseInt(path[5]);
+                let rowsPerPage = parseInt(path[6]);
+                
+                await handleGetUserCompanies(res, userId, keyword, 
+                    orderBy !== '-' ? orderBy : undefined , 
+                    ascOrDesc !== '-' ? ascOrDesc : undefined, 
+                    isNaN(pageNum) ? 1 : pageNum, isNaN(rowsPerPage) ? 10 : rowsPerPage);
+
+            }
+            else {
+                await handleGetUserCompany(res,  param1, userId);
+            }
+     
+        }
+        else {
+            res.status(400).json({message: "Invalid action!"});
+        }
+
+    }
+
+}
+
+
+
+async function handleGetUserCompanies ( 
+    res: NextApiResponse, 
+    userId? : string,  
+    keyword? : string, 
+    orderBy? : string, 
+    ascOrDesc? : string, 
+    page? : number, 
+    rowsPerPage? : number  ){
+
+    try {
+
+        let p = getPageOffsetAndLimit(page ?? 1, rowsPerPage ?? 10);
+
+        let ndata = await getCompanies(userId ?? "", keyword, orderBy, ascOrDesc, p.offset, p.limit);
+
+        let data = {...ndata, page, rowsPerPage};
+        data = { ...data , totalPages : toTotalPages(data)  };
+        //console.log("data:::",data);
+        res.status(200).json({  data : data, status : 1});   
+
+    }
+    catch(e: any){
+        console.log("e::",e);
+        res.status(422).send({error: e.message, status: -1});
+    }
+}
+
+
+async function handleGetUserCompany ( res: NextApiResponse, id : string,  userId? : string  ){
+
+    try {
+
+        let ndata = await getUserCompany(userId ?? "");
+
+        if ( ndata !== undefined && ndata !== null ){
+            res.status(200).json({  data : ndata, status : 1});   
+        }else {
+            res.status(404).json({  message : "Contact NOT found", status : -1});      
+        }
+    }
+    catch(e: any){
+        console.log("e::",e);
+        res.status(422).send({error: e.message, status: -1});
+    }
+}
+
+
+
+
+async function handlePost (req: NextApiRequest,  res: NextApiResponse, userId? : string) {
+    
+    const { path } = req.query;
+
+    if ( !userId) {
+        res.status(400).json({message: "Invalid user id!"});
+        return; 
+    }
+
+    if (Array.isArray(path) && path.length > 1) {   
+
+        const param1 = path[1];
+        jsonParser(req, res, async () => {
+            const { data } = req.body;
+            
+            if ( data !== undefined) {
+                
+                if ( param1 === 'create'){
+                    await handleCreate(res, userId ,  data);
+                } else if ( param1 === 'update'){
+                    await handleUpdate(res, userId ,  data);
+                } else {
+                    res.status(400).json({message: "Invalid action!"});
+                }
+            }
+            else {
+             
+                res.status(400).json({message: "NO proper body data provided!"});
+              
+            }
+        });
+
+    }
+    
+}
+
+
+
+
+async function handleCreate ( res: NextApiResponse,  userId: string, data : any  ){
+
+    try {
+
+        let ndata = await createCompany(userId, data );
+
+        res.status(200).json({ message: "Created", data : ndata, status : 1});   
+
+    }
+    catch(e: any){
+        res.status(422).send({error: e.message, status: -1});
+    }
+}
+
+
+
+
+async function handleUpdate ( res: NextApiResponse, userId : string, usercompany : any  ){
+
+    try {
+
+        let updated = await updateCompany(userId, usercompany );
+
+        res.status (200).send({status: updated ? 1 : -1, message : updated ? 'Record successfully updated' : 'Failed to update record'});
+
+    }
+    catch(e: any){
+        console.log("e::",e);
+        res.status(422).send({error: e.message, status: -1});
+    }
+}
+
+
+async function handleDelete (req: NextApiRequest,  res: NextApiResponse, userId? : string ) {
+    const { path } = req.query;
+
+    if (Array.isArray(path) && path.length > 1) {   
+
+        const param1 = path[1];
+
+        if ( param1 !== undefined && param1 !== null ){
+            await handleDeleteUserCompany(res, userId, param1);
+        }else {
+            res.status(400).json({message: "Invalid action!"});
+        }
+
+    }
+
+}
+
+
+async function handleDeleteUserCompany ( res: NextApiResponse, userId? : string,  id? : string  ){
+
+    try {
+
+        let d = await deleteCompany(userId ?? "", id ?? "");
+        
+        res.status(200).json({  deleted : d, status : 1});   
+
+    }
+    catch(e: any){
+        console.log("e::",e);
+        res.status(422).send({error: e.message, status: -1});
+    }
+}
