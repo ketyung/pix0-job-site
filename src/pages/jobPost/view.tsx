314156ad07da5ee443733a06edf103d537cc8ce1
ketyung <ketyung@techchee.com>
2024-04-17 14:28:33 +0800

    update

--- src/pages/jobPost/view.tsx
+++ src/pages/jobPost/view.tsx
@@ -1,6 +1,8 @@
 import MarkdownRenderer from "@/components/MarkdownRenderer";
 import { JobStatus } from "@/models";
 import Link from "next/link";
+import { BsLightningCharge } from "react-icons/bs";
+import { GoLinkExternal } from "react-icons/go";
 
 type props = {
     jobPost?: any, 
@@ -14,9 +16,9 @@ export default function View({ jobPost } :props) {
 
         return (jobPost?.jobStatus === JobStatus.Published ?
         <Link href={jobPost.applyAtExt === 'Y' ? (jobPost.applyAtUrl ?? "/") : "/" } 
-        target="_blank"><div className={`rounded-2xl px-4 text-gray-100 bg-${jobPost.applyAtExt === 'Y' ? 'orange' : 'blue'}-500 
-        w-${jobPost.applyAtExt === 'Y' ? '64' :'48'} text-center py-1`}>
-        {jobPost.applyAtExt==='Y' ? 'Apply At External Site' : "Easy Apply"}</div></Link> : <></>);
+        target="_blank"><div className={`flex rounded-2xl px-4 text-gray-100 bg-${jobPost.applyAtExt === 'Y' ? 'orange' : 'blue'}-500 
+        w-${jobPost.applyAtExt === 'Y' ? '96' :'48'} text-center py-1`}>
+        {jobPost.applyAtExt==='Y' ? <><GoLinkExternal className="ml-14 w-5 h-5 mr-1 mt-1"/>Apply At External Site</> : <><BsLightningCharge className="ml-2 w-5 h-5 mr-1 mt-1"/>Easy Apply</>}</div></Link> : <></>);
     }
 
     return <div className="mt-2 border border-gray-300 rounded p-2 lg:w-3/5 w-11/12 mx-auto lg:mb-2 mb-20 shadow-xl">
