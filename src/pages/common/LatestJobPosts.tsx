02b083823828c3746805cf085bb4cab0d3854733
ketyung <ketyung@techchee.com>
2024-04-17 12:06:40 +0800

    update

--- src/pages/common/LatestJobPosts.tsx
+++ src/pages/common/LatestJobPosts.tsx
@@ -1,4 +1,5 @@
 import ProfileImage from "@/components/ProfileImage";
+import { JobCategorys } from "@/models";
 import { ntb } from "@/utils";
 
 type props ={
@@ -8,19 +9,33 @@ type props ={
 export default function LatestJobPosts ({jobPosts }: props){
 
 
-    const jobCatDiv = (jCat : any ) =>{
+    const jobCatBgColor = (jCat : any ) =>{
 
+        let colors = ["red", "green", "blue",  "yellow", "green","gray", "purple"  ];
 
+        let idx = JobCategorys.indexOf(jCat);
+
+        if ( idx >=0 && idx < colors.length){
+
+            let rtColor= colors[idx];
+          
+            return rtColor;
+        }
+        return 'black';
     }
 
     return <div className="mt-10 lg:w-3/5 w-4/5 mx-auto">
-        <div className="text-2xl my-2">Latest Job Posts</div>      
+        <div className="text-2xl my-2 ml-2">Latest Job Posts</div>      
         {jobPosts.results.map((j:any)=>{
+
+            let catCls = `lg:w-32 w-24 text-center lg:text-base text-xs ml-2 lg:h-8 h-6 bg-${jobCatBgColor(j.jobCategory)}-500 text-gray-100 p-1 rounded`;
+
+            //console.log("catClas::",catCls);
 
             return <div className="mb-4 border-b border-gray-300 dark:border-gray-600 py-2 hover:bg-yellow-100 hover:dark:bg-gray-700 p-2">
                 <div className="flex">
                     <div className="w-9/12 text-xl">{j.title}</div>
-                    <div className="lg:w-32 w-24 text-center lg:text-base text-xs ml-2 lg:h-8 h-6 bg-gray-500 text-gray-100 p-1 rounded">{j.jobCategory}</div>
+                    <div className={catCls}>{j.jobCategory}</div>
                 </div>
                 <div className="mt-2 flex">
                     <ProfileImage width="30px" imageUrl={j.company.logoUrl !== null ? j.company.logoUrl : undefined}  
