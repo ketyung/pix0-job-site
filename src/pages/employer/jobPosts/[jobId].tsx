33d76ea089f3904579e4884be5fba04ba7c8299a
ketyung <ketyung@techchee.com>
2024-04-17 12:22:21 +0800

    update

--- src/pages/employer/jobPosts/[jobId].tsx
+++ src/pages/employer/jobPosts/[jobId].tsx
@@ -7,7 +7,7 @@ export default function JobPostPreview() {
     const router = useRouter();
     const { jobId } = router.query;
 
-    return <MainIndex title={`Job Post Preview : ${jobId}`}>
+    return <MainIndex title={`Job Post Preview : ${jobId ?? '...'}`}>
         <View jobId={jobId}/>
     </MainIndex>
 }
