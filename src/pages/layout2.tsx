7f4926db036d4a95de8103f4df89e680904a55a4
ketyung <ketyung@techchee.com>
2024-04-18 09:46:52 +0800

    update

--- src/pages/layout2.tsx
+++ src/pages/layout2.tsx
@@ -6,6 +6,7 @@ import { ThemeToggle } from 'pix0-core-ui';
 import Link from 'next/link';
 import Head from 'next/head';
 import DefaultMain from './DefaultMain';
+import { IoHomeOutline } from "react-icons/io5";
 
 type props = {
 
@@ -30,8 +31,9 @@ export default function Layout({ title, description, children, data} : props) {
         </Head>
         <header className='dark:bg-gray-800 bg-top-bar-blue dark:text-gray-100 text-gray-800 shadow-xl 
         border-width:1px dark:border-0 border border-gray-300 dark:border-gray-800 flex'>   
-            <Link href="/employer/jobPosts" className='bg-gray-700 rounded-full text-gray-100 py-1 px-4 text-sm font-bold m-2 hover:bg-green-900 cursor-pointer'>Post A JOB</Link>
-            <ThemeToggle iconLightTextColor='#ffa'/>
+            <Link href="/" className='ml-4 mr-2 mt-2.5' title="Back To Home"><IoHomeOutline className='w-5 h-5 dark:text-gray-300 text-gray-200'/></Link>
+            <Link href="/employer/jobPosts" className='text-center bg-gray-700 rounded-full text-gray-100 py-1 px-4 text-sm font-bold m-2 hover:bg-green-900 cursor-pointer'>Post A JOB</Link>
+            <div className='w-1/12 mt-3 text-right'><ThemeToggle iconLightTextColor='#ffa'/></div>
         </header>
         { children ? <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-100 text-gray-900 min-h-screen h-full pt-4 pb-4">
             {children}</div> : DefaultMain(data )}
